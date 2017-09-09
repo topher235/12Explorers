b2d87baf9d5134af3eb279cfa7899c7cb6223e7f
Topher <christopher.hurt.15@cnu.edu>
2017-09-08 18:31:39 -0700

    Initial commit

---
+++ app/src/main/java/churt/a12explorers/Main/MainActivity.java
@@ -0,0 +1,15 @@
+package churt.a12explorers.Main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import churt.a12explorers.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
